e8644abebd7c06ea71a42b5676914588adfe91ff
Frederick A. Gonzalez <g7.alexander@gmail.com>
2023-02-27 14:34:47 -0400

    implemented product creation with error handler

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -40,9 +41,59 @@ class ProductController extends Controller
         return $response;
     }
 
-    public function create()
+    public function create(Request $request)
     {
-        return 'new Products';
+        $params_array = array(
+            'name'              => $request->name,
+            'description'       => $request->description,
+            'price'             => $request->price,
+        );
+
+        $params = (object) $params_array;
+        if (!empty($params) && !empty($params_array)) {
+
+            // validamos los datos
+            $validate = Validator::make($params_array, [
+                'name'          => 'required',
+                'description'   => 'required',
+                'price'         => 'required|numeric',
+            ]);
+
+            if (!$validate->fails()) {
+                $product = new Product();
+
+                $product->name                  = $params->name;
+                $product->description           = $params->description;
+                $product->price                 = $params->price;
+                $product->save();
+
+                $data = response()->json(array(
+                    'status'    => 'success',
+                    'code'      => 200,
+                    'message'   => 'Registro exitoso',
+                ), 200);
+            } else {
+                // Validaciones fallan
+                $data = response()->json(array(
+                    'status'    => 'error',
+                    'code'      => 400,
+                    'message'   => 'Ha ocurrido un problema con la validación de los datos',
+                    'errors'    => $validate->errors()
+                ), 404);
+            }
+        } else {
+            // Request vacio
+            $data = response()->json(
+                array(
+                    'status'    => 'error',
+                    'code'      => 404,
+                    'message'   => 'No se han recibido los datos',
+                    'datos'     => $request->input()
+                ),
+                404
+            );
+        }
+        return $data;
     }
 
     public function edit()
