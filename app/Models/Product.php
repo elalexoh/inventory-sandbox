d4cd0d1c5a84e6765cb68fe6261a40540c3e6c8a
Frederick A. Gonzalez <g7.alexander@gmail.com>
2023-02-28 13:23:32 -0400

    implemented relation with store

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -16,5 +17,11 @@ class Product extends Model
         'name',
         'description',
         'price',
+        'store_id',
     ];
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
