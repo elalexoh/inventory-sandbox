e3d55174496edb464a900d9f94e59aacf918143b
Frederick A. Gonzalez <g7.alexander@gmail.com>
2023-02-28 13:23:53 -0400

    implemented relation with products and base props

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Store extends Model
 {
     use HasFactory;
+
+    protected   $table      = 'stores';
+    public      $timestamps = false;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->HasMany(Product::class);
+    }
 }
